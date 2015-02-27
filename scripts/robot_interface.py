ed0c78a2953f788d4b79926a4ed10d2f08f8e72d
LiZhuo328 <zl347@cornell.edu>
2015-02-26 22:10:14 -0500

    Update robot_interface.py

--- scripts/robot_interface.py
+++ scripts/robot_interface.py
@@ -10,9 +10,11 @@
 
 import rospy
 import baxter_interface
+import tf2_ros
+# import roslib
 from bax_hw3.msg import *
-from bax_hw3.srv import *
-from waiter import Waiter
+# from bax_hw3.srv import *
+# from waiter import Waiter
 
 
 # The baxter class.
@@ -21,7 +23,10 @@ class Baxter():
     # Method for on-lining baxter
     # All required calls from baxter_interface
     def __init__(self):
-        self.start #?
+        self.start #What does start do???
+
+        # How do we distinguish left hand and right hand ???
+        gripper = Pose( Point(0,0,3), Quaternion(0,1,0, pi/2) )
 
         right_arm = baxter_interface.Limb('right')
         left_arm = baxter_interface.Limb('left')
@@ -29,32 +34,51 @@ class Baxter():
 
     # Method for getting joint configuration
     # Direct call to baxter_interface
-    def getJoints(self):
-        self.getJoints('left') = left_arm.joint_angles()
-        self.getJoints('right') = right_arm.joint_angles()
+    def getJoints(self,limbSide):
+        # Returns: dict({str:float})
+        # unordered dict of joint name Keys to angle (rad) Values
+
+        if limbSide == 'left':
+            self.leftCongif = left_arm.joint_angles()
+            return self.leftCongif
+        elif limbSide == 'right':
+            self.rightCongif = right_arm.joint_angles()
+            return self.rightCongif
+        else:
+            rospy.logwarn('Invalid limb side name #: ' + str(limbSide))
 
 
     # Method for getting end-effector position
     # Uses forward kinematics ROS library, TF
     # Angular pose will always be top-down, so wrist-gripper displacement doesn't have to be factored in
-    def getEndPose(self):
+    def getEndPose(self,limbSide):
         # pose = {'position': (x, y, z), 'orientation': (x, y, z, w)}
-        self.getEndPose('left') = left_arm.endpoint_pose()
-        self.getEndPose('right') = right_arm.endpoint_pose()
+
+        if limbSide == 'left':
+            self.leftEndPose = left_arm.endpoint_pose()
+            return self.leftEndPose
+        elif limbSide == 'right':
+            self.rightEndPose = right_arm.endpoint_pose()
+            return self.rightEndPose
+        else:
+            rospy.logwarn('Invalid limb side name #: ' + str(limbSide))
 
 
 
     # Method for setting joint positions
     # Direct call to baxter_interface
     # set_left and set_right are dict({str:float}), same size as joint angles
-    def setJoints(self,set_left,set_right):
+    def setJoints(self,limbSide,set_positions):
         # set_joint_positions(self, positions, raw=False)
         # positions (dict({str:float})) - joint_name:angle command
         # raw (bool) - advanced, direct position control mode
-        self.setJoints('left') = left_arm.move_to_joint_positions(set_left,raw=False)
-        self.setJoints('right') = right_arm.move_to_joint_positions(set_right,raw=False)
 
-
+        if limbSide == 'left':
+            left_arm.move_to_joint_positions(set_positions,raw=False)
+        elif limbSide == 'right':
+            right_arm.move_to_joint_positions(set_positions,raw=False)
+        else:
+            rospy.logwarn('Invalid limb side name #: ' + str(limbSide))
 
 
     # Method for setting cartesian position of hand
@@ -72,19 +96,18 @@ class Baxter():
 
 
 
-
+# Or Class robot_interface ???
 def robot_interface():
 
     
 
     # Declare that we'll be publishing to the state topic
     self.statePub = rospy.Publisher('state', State, queue_size = 10)
-
+    self.statePub = rospy.Publisher('state', State, queue_size = 10)
 
 
     # Declare that we'll be handling the MoveRobot service
     rospy.Service('pick_place', PickPlace, self.handlePickPlace)
-
 
 
     # Initializes the node (connects to roscore)
