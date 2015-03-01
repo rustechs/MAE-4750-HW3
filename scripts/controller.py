9a6a1b27c125bef24bb512bd6a130322283c88bc
CS4752 <cs4752@en-cs-lgg03-10.coecis.cornell.edu>
2015-03-01 16:43:03 -0500

    Fixing bugs in controller.py

--- scripts/controller.py
+++ scripts/controller.py
@@ -50,7 +50,7 @@ class Controller():
     def __init__(self):
 
         # Load number of blocks, configuration, bimanual setting
-        print 'Getting parameters from ROS'
+        rospy.loginfo('Getting parameters from ROS')
         self.h = .044
         self.d = .08
         self.nBlocks = rospy.get_param('num_blocks')
@@ -71,6 +71,7 @@ class Controller():
             orderedBlocks.reverse()
         for i, slot in enumerate(self.stack):
             slot.eat(orderedBlocks[i])
+        rospy.loginfo('Initial condition set to %s' % initstr) 
 
         # Initialize valid table slots - nBlocks slots on either side.
         # Organize in a dictionary.
@@ -87,6 +88,7 @@ class Controller():
         self.hasStack = 'left'
         self.busy = {'left': False, 'right': False}
         self.busy['right'] = not self.bimanual
+        rospy.loginfo('Bimanual operation: %s' % self.bimanual)
 
         # Initialize "done" indicator
         self.done = False
@@ -94,22 +96,19 @@ class Controller():
         # initialize the Baxter object and home it.
         # Note that Baxter should be initialized with his left gripper
         # grasping the top-most block of the initial stack.
-        print 'Connecting to Baxter...'
+        rospy.loginfo('Connecting to Baxter...')
         self.baxter = robot_interface.Baxter()
         self.baxter.enable()
-        print 'Connecting to Baxter successful.'
+        rospy.loginfo('Connecting to Baxter successful.')
 
         # Initialize the objective by sending a message to self
         self.handleCommand( Command(initstr) )
 
         # This node subscribes to the "command" topic.
         rospy.Subscriber("commandme", Command, self.handleCommand)
-        print 'Subscriptions successful.'
+        rospy.loginfo('Subscriptions successful.')
 
-        # Initializes the node (connects to roscore)
-        # Setting anonymous to True guaruntees unique node name
-        # NOTE: ALREADY CALLED IN BAXTER() CONSTRUCTOR
-        # rospy.init_node('controller_hw3', anonymous = True)
+        # Initializes the node (connects to roscore) in Baxter constructor.
 
     # This is the callback for processing incoming command messages.
     def handleCommand(self, cmd):
@@ -129,6 +128,7 @@ class Controller():
     # thread. It toggles flags in the Controller object.
     def pickPlaceThread(self, side, pick, place):
 
+        rospy.loginfo('Initiate pick from %s to %s with %s' % pick, place, side)
         # Set up collision/busy flags
         self.busy[side] = True
 
@@ -151,6 +151,7 @@ class Controller():
 
         # Mark the limb as available and return
         self.busy[side] = False
+        rospy.loginfo('Pick from %s to %s with %s complete.' % pick, place, side)
 
     # Convenience function for a simple move.
     # Collision avoidance is implemented here. If the destination is the stack
@@ -183,6 +184,7 @@ class Controller():
     def pickPlace(self, block, slot):
 
         # Find or wait for an available limb
+        rospy.loginfo('Waiting for limb to become available...')
         while True:
             if not self.busy['left']:
                 side = 'left'
@@ -190,6 +192,7 @@ class Controller():
             if not self.busy['right']:
                 side = 'right'
                 break
+        rospy.loginfo('Limb %s ready for pick-place.' % side)
 
         # If slot is an actual slot, get its point. If it was 'table', find
         # a slot on the table for that side.
@@ -214,16 +217,13 @@ class Controller():
         # Start planning out move right away.
 
         # If the stack order matches the objective, express victory
-        order = [slot.contains.n for slot in self.stack]
-        #debu
-        rp = self.baxter.getEndPose('right')
-        self.baxter.setEndPose('right', rp)
-        #import pdb; pdb.set_trace()
-        if order == self.objective:
-            rospy.loginfo('Objective achieved.')
-            self.baxter.face('happy')
-            self.done = True
-            return
+        if not (None in [slot.contains for slot in self.stack]):
+            order = [slot.contains.n for slot in self.stack]
+            if order == self.objective:
+                rospy.loginfo('Objective achieved.')
+                self.baxter.face('happy')
+                self.done = True
+                return
 
         # If it is not done, travel up the stack looking for the first stack
         # slot that either contains the wrong block or is empty.
@@ -239,7 +239,11 @@ class Controller():
             # If the slot contains the wrong block, take the top of the stack # off and place it on an available table slot.
             if not (slot.contains is rightBlock):
                 nums = [self.stack[i].contains for i in range(self.nBlocks)]
-                removeBlock = self.blocks[nums.index(None) - 1]
+                try:
+                    indn = nums.index(None) - 1
+                except:
+                    indn = len(nums) - 1
+                removeBlock = self.blocks[indn]
                 self.pickPlace(removeBlock, 'table')
                 return
 
