5454afd7c3f7b1e5f0e91b6190bf275315e1e1c0
amcnicoll <anthony.mcnicoll@gmail.com>
2015-02-28 18:45:02 -0600

    Work checkpoint on controller.py

--- scripts/controller.py
+++ scripts/controller.py
@@ -10,55 +10,99 @@
     moves and coordinate limb motion.
 '''
 
-import rospy
+import rospy, copy
 from bax_hw3.msg import *
 from bax_hw3.srv import *
 from geometry_msgs.msg import Point, Quaternion, Pose
 from waiter import Waiter
 
+# A simple Block class for legibility.
+class Block():
+    def __init__(self, number, x = 0, y = 0, z = 0):
+        self.pos = Point(x, y, z)
+        self.n = number
+
+# A class representing an allowed "slot" for a block in the workspace.
+class Slot():
+    def __init__(self, x, y, z):
+        self.pos = Point(x, y, z)
+        self.contains = None
+
+    # Change a Block's coordinates to match this slot's
+    def eat(self, block):
+        block.pos = self.pos
+        self.contains = block
+
+    # "Lets go" of a block
+    def vomit(self):
+        self.contains = None
+
 # The controller class.
 class Controller():
 
-    # This is the main controller function. After initializing its subscriber
-    # status and setting up a service proxy, it continually works until the
-    # specified objective is achieved.
+    # Initializes the controller as a ROS node which recieves commands.
     def __init__(self):
 
-        # Load the number of blocks and default configuration
+        # Load number of blocks, configuration, bimanual setting
+        h = .044
+        d = .08
         self.nBlocks = rospy.get_param('num_blocks')
         initstr = rospy.get_param('configuration')
+        self.bimanual = (1 == rospy.get_param('bimanual'))
+
+        # Initialize the block set and stack list together.
+        # The stack list is a list of (occupied) slots.
+        y = [.044*i for i in range(self.nBlocks)]
+        if initstr == 'stacked_descending':
+            y.reverse()
+        self.blocks = [Block(i) for i in range(self.nBlocks)]
+        self.stack = [Slot(0, 0, y[i]) for i in range(self.nBlocks)]
+        for i, slot in enumerate(self.stack):
+            slot.eat(self.blocks[i])
+
+        # Initialize valid table slots
+        #   If unimanual, add slots extending left of the stack.
+        #   If bimanual, add slots to left for even indices, right for odd
+        if self.bimanual:
+            self.slots = []
+            for i in range(self.nBlocks):
+                s = -1 + 2*(i%2)
+                m = 1 + i/2
+                self.slots.append(Slot(s*d*m, 0, 0))
+        else:
+            self.slots = [Slot(-(i+1)*d, 0, 0) for i in range(self.nBlocks)]
+
+        # Initialize a collision prevention variable called "hasStack", which
+        # at all times should be maintained with either 'left', 'right', or
+        # None values to indicate who has control of the stack.
+        self.hasStack = 'left'
+
+        # TODO initialize the Baxter object and home it.
 
         # Initialize the objective by sending a message to self
         self.handleCommand( Command(initstr) )
 
-        # This node subscribes to the "command" and "state" topics.
-        # Use a MsgBuffer to queue up the state messages.
+        # This node subscribes to the "command" topic.
         rospy.Subscriber("command", Command, self.handleCommand)
-        self.stateWaiter = Waiter()
-        rospy.Subscriber("state", State, self.stateWaiter.cb)
-
-        # Set up the service proxy for sending commands to robot
-        self.moveRobot = rospy.ServiceProxy('MoveRobot', MoveRobot)
 
         # Initializes the node (connects to roscore)
         # Setting anonymous to True guaruntees unique node name
-        rospy.init_node('controller_node', anonymous = True)
+        rospy.init_node('controller_hw3', anonymous = True)
 
     # This is the callback for processing incoming command messages.
     def handleCommand(self, cmd):
 
-        # Get the command and create the objective
-        # This has code similar to sim_master initialization code
+        # Get the command and create the objective.
+        # The objective is encoded as a numerical array of block numbers in
+        # the stack, from bottom to top.
         initstr = cmd.command
-        if initstr == 'scattered':
-            self.objective = [0 for i in range(self.nBlocks)]
-        elif initstr == 'stacked_ascending':
-            self.objective = range(self.nBlocks)
-        elif initstr == 'stacked_descending':
-            self.objective = range(2, self.nBlocks + 1) + [0]
+        self.objective = range(self.nBlocks)
+        if initstr == 'stacked_descending':
+            self.objective.reverse()
         rospy.loginfo('Objective has changed to ' + initstr)
 
-    # Function that takes action depending on the state of the workspace
+    # Function that takes action depending on the state of the workspace.
+    # All the secret sauce is in here.
     def planMove(self):
 
         # Work with a fresh state.
@@ -91,28 +135,6 @@ class Controller():
         self.moveRobot('close',0)
         self.moveRobot('move_over', targOn)
         self.moveRobot('open',0)
-
-    def chainUp(self, block, stateArr):
-        above = self.isBelow(block, stateArr)
-        if above is None:
-            return block
-        else:
-            return self.chainUp(above, stateArr)
-
-    # Returns the block above block
-    def isBelow(self, block, stateArr):
-        try:
-            out = stateArr.index(block) + 1
-        except ValueError:
-            out = None
-        return out
-
-    # Returns the block below block
-    def isAbove(self, block, stateArr):
-        return stateArr[block - 1]
-
-    def isInPosition(self, block, stateArr):
-        return stateArr[block - 1] is self.objective[block - 1]
 
 # This is what runs when the script is executed externally.
 # It runs the main node function, and catches exceptions.
